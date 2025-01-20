4da0f7a0cf3ce9a041f8c6ae64d951b76f74a5c9
kirasafferstein <ksaffers@uvm.edu>
2025-01-20 10:13:07 -0500

    Stepped the physics, included time, printed iterations

--- simulate.py
+++ simulate.py
@@ -1,4 +1,16 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
+
+# Slow things down to see simulated world 
+for i in range(1000):
+	p.stepSimulation()
+
+	# Sleep the code by 1/60th of a second during each pass
+	time.sleep(1/60)	
+	
+	# Print value of for loop
+	print(f"Iteration number: {i}")	
+
 p.disconnect()
