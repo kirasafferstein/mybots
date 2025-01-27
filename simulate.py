8ccbbcbe17da3cf8ca5af48fc5045875c55fa2cf
kirasafferstein <ksaffers@uvm.edu>
2025-01-27 18:06:04 -0500

    Created 25 tower

--- simulate.py
+++ simulate.py
@@ -1,10 +1,20 @@
 import pybullet as p
 import time
+import pybullet_data #Tell pybullet where to find .urdf
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+
+# Insert forces (gravity)
+p.setGravity(0,0,-9.8)
+
+# Add a floor for the box to collide with
+planeId = p.loadURDF("plane.urdf")
+
 
 # Read in the world described in box.sdf
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 
 # Slow things down to see simulated world 
